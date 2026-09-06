3a9fe04aa05daee8c54195759f1b8be5c2665f69
yueyinqiu <yueyinqiu@outlook.com>
2026-09-06 14:26:19 +0800

    ww

---
+++ src/EasytierProxy.Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using CliFx;
+using CliFx.Binding;
+using CliFx.Infrastructure;
+
+namespace Snavi;
+
+[Command("run")]
+public partial class RunCommand : ICommand
+{
+    [CommandOption("easytier-core")]
+    public string EasytierCore { get; set; } = "easytier-core";
+
+    [CommandOption("easytier-cli")]
+    public string EasytierCli { get; set; } = "easytier-cli";
+
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        
+    }
+}
